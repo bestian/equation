9bdb47eaecfd94f12a59597aaa5665ecf1690e1f
Bestian Tang <bestian@gmail.com>
2020-10-20 19:59:23 +0800

    初探

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,7 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do print $ "Enter an Equation"
+          x <- getLine
+          let eq = parseEq x
+          print $ solve eq
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,49 @@
 module Lib
-    ( someFunc
+    ( Equation (Q, L), solve, parseEq,
+      splitBy, sp, r, rm, rm1
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Equation = Q Float Float Float | L Float Float deriving (Show, Read)
+
+solve :: Equation -> [Float]
+solve (Q a b c) = [x1, x2]
+    where x1 = (- b + sqrt d) / (2 * a)
+          x2 = (- b - sqrt d) / (2 * a)
+          d = b ** 2 - 4 * a * c
+
+solve (L a b) = [x]
+    where x = - b / a
+
+splitBy :: Char -> [Char] -> [[Char]]
+splitBy c [] = []
+splitBy c (x:xs) = sp c [] (x:xs)
+
+sp :: Char -> [Char] -> [Char] -> [[Char]]
+sp c ks [] = ks : []
+sp c ks (x:xs) | x == c    = ks : sp c [] xs
+               | otherwise = sp c (ks ++ [x]) xs
+
+r :: [[Char]] -> Int -> Float
+r list k = (read (list !! k) :: Float)
+
+rm :: [Char] -> [Char] -> [Char]
+rm _ [] = []
+rm [] xs = xs
+rm (a:as) (x:xs) | a == x    = rm1 as xs (x:xs)
+                 | otherwise = x : rm (a:as) xs
+
+rm1  :: [Char] -> [Char] -> [Char] -> [Char]
+rm1 [] [] os = os
+rm1 _ [] os = os
+rm1 [] xs os = xs
+rm1 (a:as) (x:xs) os | a == x    = rm1 as xs os
+                     | otherwise = os
+
+parseEq [] = L 1 0
+parseEq a@(x:xs) | length (list) == 2  = L (r list 0) (r list 1)
+                 | length (list) == 3  = Q (r list 0) (r list 1) (r list 2)
+                 | otherwise           = L 1 0
+    where list = splitBy '+' pretty
+          pretty = (filter (/= ' ') . filter (/= '=') . filter (/= '0') . filter (/= 'x')) a1
+          -- a1 = a
+          a1 = rm "^2" a
